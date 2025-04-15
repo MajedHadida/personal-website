13acaab629c15c4d36e8c6573ba593ae3538773d
Majed Hadida <majedhadida@hotmail.com>
2025-04-15 15:19:35 -0400

    cloud css finally!

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
     return(
         <div className='bg-primary h-full flex flex-col justify-center align-middle'>
             <div className='grid grid-cols-3 grid-rows-3 mx-28 gap-10 my-3' id='body'>
-                <div className='flex flex-col items-center container bg-base-100 col-span-2 row-span-3 rounded-lg p-5'>
+                <div className='flex flex-col items-center container bg-base-100 col-span-2 row-span-3 rounded-lg p-5 z-10'>
                 <h1 className='text-5xl font-bold'>My Pixel Art!</h1>
                 <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
                 <div>
@@ -65,11 +65,11 @@ const Contact = () => {
                 </div>
                 </div>
                 </div>
-                <div className='flex flex-col container bg-base-100 col-span-1 row-span-1 rounded-lg p-5'>
+                <div className='flex flex-col container bg-base-100 col-span-1 row-span-1 rounded-lg p-5 z-10'>
                     <h1 className='text-3xl text-left'>A little about me</h1>
                     <p>Nature lover and a pixel art enthusiast. I have a minor in UX design, so I love making user-centric designs. If I'm not coding, you'll find me playing video games (usually cozy games or MMORPGs... <i>add me on Steam!</i>), doing pixel art, or out on a hike!</p>
                 </div>
-                <div className='flex flex-col container bg-base-100 col-span-1 row-span-2 rounded-lg p-5'>
+                <div className='flex flex-col container bg-base-100 col-span-1 row-span-2 rounded-lg p-5 z-10'>
                     <div className='flex flex-col'>
                         <h1 className='text-3xl text-left'>Currently working on...</h1>
                         <div className='flex items-center justify-center bg-secondary w-full rounded-3xl mt-5'>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,6 +24,7 @@ const Home = () =>{
 //       observer.disconnect()
 //     };
 //   }, []);  
+    const [showCloud2, setShowCloud2] = useState(false);
 
 
     React.useEffect(() => {
@@ -34,35 +35,41 @@ const Home = () =>{
                 const computedStyle = getComputedStyle(el);
                 console.log('Value of --rand:', computedStyle.getPropertyValue('--rand'));
             }
+            const timer = setTimeout(() => {
+                setShowCloud2(true);
+            }, 5000);
+            return () => clearTimeout(timer);
         };
     }, []);
 
     function setCloud(){
         console.log("onanimationend")
-        let el = document.getElementById('cloud');
-        if(el){
-            el.style.animation = "none";
-            el.style.setProperty("--rand", Math.random());
-            el.style.animation = "fade-right 2s ease-out, moveClouds 60s linear"
-        }
+        // let el = document.getElementById('cloud');
+        // if(el){
+        //     el.style.setProperty("--rand", Math.random());
+        //     el.style.animation = "fade-right 2s ease-out, moveClouds 60s linear"
+        // }
     }
 
     return (
         <div className='bg-primary overflow-x-hidden relative'>
             <Navigationbar/>
-            <div className="cloud absolute" id="cloud" onAnimationEnd={setCloud}></div>
+            <div className="cloud absolute delay-[5000]" id="cloud" onAnimationEnd={setCloud}></div>
+            {showCloud2 && (
+                <div className="cloud absolute" id="cloud2" onAnimationEnd={setCloud}></div>
+            )}
             <div id="main" className='flex justify-center align-middle'>
                 <div className='flex flex-col min-h-screen justify-center' id='body'>
                     <div className='text-3xl'>
-                    <p className="animate-fade-right-3s animate-duration">Hello, I'm</p>
+                    <p className="animate-fade-right-3s animate-duration z-10">Hello, I'm</p>
                     </div>
                     <div>
-                    <h1 className='text-9xl font-bold animate-fade-right-1s'>Majed</h1>
+                    <h1 className='text-9xl font-bold animate-fade-right-1s z-10'>Majed</h1>
                     </div>
                     <div className=''>
-                    <p className='text-4xl animate-fade-right-3s'>I'm a front-end developer.</p>
+                    <p className='text-4xl animate-fade-right-3s z-10'>I'm a front-end developer.</p>
                     </div>
-                    <div className='animate-fade-right-3s'>
+                    <div className='animate-fade-right-3s z-10'>
                     <span className='text-2xl italic'>Aspiring to be a full-stack developer </span><span>😁</span>
                     </div>
                 </div>
